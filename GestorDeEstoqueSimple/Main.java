7db69781d04232bb3b5777ee1bf4f5e8d2ed6db6
Alexandre <alexandreflxx@gmail.com>
2026-02-05 00:57:34 +0000

    Meu primeiro commit no codespace

---
+++ GestorDeEstoqueSimple/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+    public class Main {
+        Scanner scanner = new Scanner(System.in); //criando um Scanner
+        int opcao = 0;
+        
+        public static void main(String[] args) {
+
+            while (opcao != 4) {                            //While para Lista o menu
+            System.out.println("\n--- MENU ---");
+            System.out.println("1 - Adicionar Produto");
+            System.out.println("2 - Listar produto");
+            System.out.println("3 - Remover produto");
+            System.out.print("4 - Sair");
+
+            if (scanner.hasNextInt) 
+
+
+        }
+
+
+    }
