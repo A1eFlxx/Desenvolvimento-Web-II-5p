0e0b284fe1c5ecdad89820ee17d31eea9161d4dd
Alexandre <alexandreflxx@gmail.com>
2026-02-26 01:03:54 +0000

    aula dia 25/02

---
+++ Aula2526/primeiroProjetoAlexandreFelix/src/main/java/com/unipam/primeiroProjetoAlexandreFelix/PrimeiroProjetoAlexandreFelixApplication.java
@@ -0,0 +1,13 @@
+package com.unipam.primeiroProjetoAlexandreFelix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoAlexandreFelixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoAlexandreFelixApplication.class, args);
+	}
+
+}
---
+++ Aula2526/primeiroProjetoAlexandreFelix/src/test/java/com/unipam/primeiroProjetoAlexandreFelix/PrimeiroProjetoAlexandreFelixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipam.primeiroProjetoAlexandreFelix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoAlexandreFelixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
